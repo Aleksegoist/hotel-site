926442a4a3bc8353e085acd45eb10e1572d78332
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-05-06 18:44:29 +0300

    added form for check in/out

---
+++ src/components/BookForm.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { RoomContext } from '../context/RoomContext';
+import AdultsDropDown from './AdultsDropDown';
+import CheckIn from './CheckIn';
+import CheckOut from './CheckOut';
+import KidsDropDown from './KidsDropDown';
+
+const BookForm = () => {
+  const { handleClick } = useContext(RoomContext);
+  return (
+    <form className='h-[300px] w-full lg:h-[70px]'>
+      <div className='flex flex-col w-full h-full lg:flex-row'>
+        <div className='flex-1 border-r'>
+          <CheckIn />
+        </div>
+        <div className='flex-1 border-r'>
+          <CheckOut />
+        </div>
+        <div className='flex-1 border-r'>
+          <AdultsDropDown />
+        </div>
+        <div className='flex-1 border-r'>
+          <KidsDropDown />
+        </div>
+        <button
+          className='btn btn-primary'
+          type='submit'
+          onClick={(e) => handleClick(e)}>
+          Check now
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default BookForm;

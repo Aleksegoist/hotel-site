991205c42da031afbf07c5433b4285ba1f034cd7
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-05-07 16:58:23 +0300

    added header log

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Logo from '../assets/img/logo.png';
 
 const Header = () => {
-  return <div>Header</div>;
+  const [header, setHeader] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+  });
+  return (
+    <header
+      className={`${
+        header ? 'bg-white py-6 shadow-lg' : 'bg-transparent py-8'
+      } fixed z-50 w-full transition-all duration-300`}>
+      <div className='container mx-auto'>
+        <a href='/'>
+          {header ? (
+            <img className='w-[160px]' src={Logo} alt='logo' />
+          ) : (
+            <img className='w-[160px]' src={Logo} alt='logo' />
+          )}
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

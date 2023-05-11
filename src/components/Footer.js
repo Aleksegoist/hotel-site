c4142ec3ba6fee013dd51471ceacb18bb75909a2
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-05-11 17:25:39 +0300

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Logo from '../assets/img/logo.png';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-primary py-12'>
+      <div className='container mx-auto text-white flex justify-between items-center'>
+        <a href='/'>
+          <img src={Logo} alt='logo img' />
+        </a>
+        Copyright &copy; 2023. All rights reserved
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
